b5ecee2f225b53d6f659c04098e7fdd362600840
Web <web@Josss-MacBook-Pro.local>
2020-01-22 10:50:53 +0000

    Mostly Working

--- main.js
+++ main.js
@@ -1,64 +1,80 @@
-const csv = require('csv-parser');
-const fs = require('fs');
+const csv = require("csv-parser");
+const fs = require("fs");
 
-const input = 'input.csv';
-const inputRows = [];
+const input = "input.csv";
+const inputData = [];
 
 fs.createReadStream(input)
-    .pipe(csv({
-        mapHeaders: ({ header }) => 
-            header
-                .toLowerCase()
-                .replace(/\s/g, "")
-    }))
-    .on('data', (data) => inputRows.push(data))
-    .on('end', () => {
-        // Success
-        //console.log("Success");
-        // inputRows array
-        //console.log(inputRows);
-        //console.log(inputRows.length);
-        // Product Name
-        const originYear = inputRows[0].originyear;
-        //console.log("Origin Year: " + originYear);
-        //Get Count of Years
-        function countUnique(iterable) {
-            return new Set(iterable).size;
-          }
-          let devYears = [];
-        inputRows.forEach(element => {
-            //console.log(element.developmentyear);
-            devYears.push(element.developmentyear);
-        })
-        const devYearNo = countUnique(devYears);
-        //console.log(countUnique(devYears));
+  .pipe(
+    csv({
+      mapHeaders: ({ header }) =>
+        header
+          // Remove whitespace from header names / lower case
+          .toLowerCase()
+          .replace(/\s/g, "")
+    })
+  )
+  .on("data", data => inputData.push(data))
+  .on("end", () => {
+    // Get the origin year - remove whitespace
+    let originYear = inputData[0].originyear.trim(" ");
 
-        // Working Out The Triangle
-        // First Product Name = Product 1, check origin year, if origin is dev then return incremental value, if origin is different to dev return prev value plus incremental value
-        const firstProduct = inputRows[0].product;
-        var i;
-        var currentAmt = 0;
-        var productArray = [];
-
-        for (i = 0; i < inputRows.length; i++) {
-            let productName = inputRows[i].product;
-            if (productName == firstProduct) {
-                if(productArray[0] != productName){
-                    productArray.push(productName);
-                }
-                if (inputRows[i].originyear == inputRows[i].developmentyear) {
-                    currentAmt = currentAmt + parseInt(inputRows[i].incrementalvalue);
-                    productArray.push(parseInt(inputRows[i].incrementalvalue));
-                } else {
-                    currentAmt = currentAmt + parseInt(inputRows[i].incrementalvalue);
-                    productArray.push(currentAmt);
-                }
-            } else {
-
-            }
-        }
-
-        //FINAL TO WRITE TO CSV
-        var arrayToList = productArray.toString() + "\r\n";
-        fs.writeFileSync('output.csv', originYear + "," + devYearNo + "\r\n" + arrayToList);
+    // Number of development years
+    function countUnique(iterable) {
+      return new Set(iterable).size;
+    }
+    let developmentYears = [];
+    inputData.forEach(element => {
+      developmentYears.push(element.developmentyear);
     });
+    const developmentYearCount = countUnique(developmentYears);
+
+    // Get name of first product
+    let firstProduct = inputData[0].product;
+
+    // Loop through input data
+    var i;
+    var outputArray = [];
+    var accumAmount;
+
+    for (i = 0; i < inputData.length; i++) {
+      // Get the name of the current product
+      let currentProduct = inputData[i].product;
+      // Check if the current product is the same as the first product
+      if (currentProduct == firstProduct) {
+        // Check whether this product is in the output array
+        formatProduct = "\n " + currentProduct;
+        if (!outputArray.includes(formatProduct)) {
+          outputArray.push(formatProduct);
+        }
+        if (inputData[i].originyear == inputData[i].developmentyear) {
+          accumAmount = parseFloat(inputData[i].incrementalvalue);
+          outputArray.push(parseFloat(inputData[i].incrementalvalue));
+        } else if (inputData[i].originyear != inputData[i].developmentyear) {
+          accumAmount = accumAmount + parseFloat(inputData[i].incrementalvalue);
+          outputArray.push(accumAmount);
+        }
+      } else if (currentProduct != firstProduct) {
+        formatProduct = "\n " + currentProduct;
+        if (!outputArray.includes(formatProduct)) {
+          outputArray.push(formatProduct);
+        }
+        if (inputData[i].originyear == inputData[i].developmentyear) {
+          accumAmount = parseFloat(inputData[i].incrementalvalue);
+          outputArray.push(parseFloat(inputData[i].incrementalvalue));
+        } else if (inputData[i].originyear != inputData[i].developmentyear) {
+          accumAmount = accumAmount + parseFloat(inputData[i].incrementalvalue);
+          outputArray.push(accumAmount);
+        }
+      }
+    }
+
+    // Format the array as a string
+    var arrayToList = outputArray.toString() + "\r\n";
+
+    // Format the output for the CSV
+    var output = originYear + ", " + developmentYearCount + arrayToList;
+
+    // Write to output file
+    fs.writeFileSync("outputs/output.csv", output);
+  });
